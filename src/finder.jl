6fe53f7cb62f0bfe0ad706abd8fbce11c9aa601b
daus-s <dauscarmichael@gmail.com>
2024-07-24 15:00:33 -0700

    formats output from ragnar data source on stdout

--- src/finder.jl
+++ src/finder.jl
@@ -1,12 +1,25 @@
 module Finder
+using Gumbo, AbstractTrees
 
-export has_class
+export has_class, get_text
+
 function has_class(node, class_name)
     println(node, class_name)
     if node[:class] !== nothing
         return class_name in split(node[:class])
     end
     return false
+end
+
+function get_text(node)
+    if node isa Gumbo.HTMLElement
+        texts = [strip(text(child)) for child in AbstractTrees.children(node)]
+        return join(texts, " ")
+    elseif node isa Gumbo.HTMLText
+        return strip(text(node))
+    else
+        return ""
+    end
 end
 
 end #module
--- src/main.jl
+++ src/main.jl
@@ -1,47 +1,58 @@
 include("finder.jl")
 
 using HTTP, Gumbo, AbstractTrees, Logging
-using .Finder: has_class
+using .Finder
 
 function getTeamLinks(htmlRoot, file)
     counter = 0
     for elem in PreOrderDFS(htmlRoot)
         try
             if tag(elem) == :table
-                if counter == 1
+                counter += 1
+                if counter == 2
                     println("result table found")
                     for child in AbstractTrees.children(elem)
                         if tag(child) == :tbody
+                            mh = []
                             for row in AbstractTrees.children(child)
                                 if tag(row) == :tr
                                     println("---------------------------------------------------------------------------------------------------------------------------------------------------------------")
-                                    for cell in AbstractTrees.children(row)
+                                    for (fieldIndex, cell) in enumerate(AbstractTrees.children(row))
+                                        if length(mh) < 7
+                                            if tag(cell) == :th
+                                                push!(mh, text(cell))
+                                                # println(mh)
+                                            end
+                                        end
                                         if tag(cell) == :td
-                                            for i in AbstractTrees.children(cell)
-                                                println(text(i))
+                                            try
+                                                print(mh[fieldIndex], ": ", get_text(cell), "\n")
+                                            catch
+                                                print(mh[fieldIndex], ": ", nothing, "\n") #out of bounds?
+                                            end
+                                            for (i, child) in enumerate(AbstractTrees.children(cell))
                                                 try
-                                                    if tag(i) == :a
+                                                    if fieldIndex == 5 && tag(child) == :a
                                                         try
-                                                            link = String(getattr(i, "href")) * "\n"
-                                                            println("link: ", link)
+                                                            link = String(getattr(child, "href")) * "\n"
+                                                            print("link: ", link)
                                                             write(file, link)
                                                         catch
                                                             println("link crash")
                                                         end
+
                                                     end
                                                 catch
-                                                    println("access crash")
+                                                    print(mh[fieldIndex], ": ", nothing, "\n")
                                                 end
                                             end
                                         end
                                     end
-                                    println("\n---------------------------------------------------------------------------------------------------------------------------------------------------------------")
                                 end
                             end
                         end
                     end
                 end
-                counter += 1
             end
         catch
             # Nothing needed here
@@ -50,11 +61,12 @@ function getTeamLinks(htmlRoot, file)
             break
         end
     end
+    println("\n---------------------------------------------------------------------------------------------------------------------------------------------------------------")
 end #function
 
 
 function getRagnarData()
-    ragnarPage = HTTP.get("https://timing.brooksee.com/nwp/results?sort=&race=167216&date=&event=&gender=&division=&search=&loc_169250=76348&page_169250=1&size_169250=100000&loc_169252=&page_169252=1&size_169252=25")
+    ragnarPage = HTTP.get("https://timing.brooksee.com/nwp/results?sort=&race=167216&date=&event=&gender=&division=&search=&loc_169250=&page_169250=1&size_169250=100000&loc_169252=&page_169252=1&size_169252=25")
     r_parsed = parsehtml(String(ragnarPage.body))
     root = r_parsed.root
     return root
